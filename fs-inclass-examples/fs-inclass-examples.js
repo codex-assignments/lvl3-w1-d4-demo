5e8dd46c248332615da156f6ad852f99f5fa0198
Ashley Flynn <ashnflynn@gmail.com>
2026-04-13 20:37:08 -0500

    class demo

---
+++ fs-inclass-examples/fs-inclass-examples.js
@@ -0,0 +1,60 @@
+"use strict";
+
+import fs from "fs";
+
+//SYNCHRONOUS OPERATIONS
+//Append to file - synchronous
+//create file or if it already exists add to end of a file each time it is run
+// fs.appendFileSync("test.txt", "Test!")
+
+//Overwrite the data of a file
+// fs.writeFileSync("test.txt", "Test!")
+
+// const data = fs.readFileSync("test.txt", "utf8")
+
+// console.log(data)
+
+// // delete file
+// fs.unlinkSync("test.txt")
+
+//ASYNCHRONOUS OPERATIONS
+
+fs.writeFile("test.txt", "Test 2", (error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log("File created!");
+});
+
+fs.readFile("test.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
+
+fs.appendFile("test.txt", "\nAnother Test", (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
+
+// fs.unlink("test.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+// });
+
+//make a new directory/folder
+fs.mkdirSync("test")
+
+fs.writeFile("./test/test.txt", "I am in a folder!", (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+})
